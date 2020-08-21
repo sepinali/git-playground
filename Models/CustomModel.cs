8736efb0c2de343d9aa6c3dd80cbbe1caf23b12f
Sina Setayesh <s.setayesh@gmail.com>
2020-08-21 21:50:43 +1000

    changes to customModel

--- Models/CustomModel.cs
+++ Models/CustomModel.cs
@@ -5,5 +5,9 @@ namespace repos.Models
     public class CustomModel{
         public string Name{get;set;}
         public string Email{get;set;}
+
+        public DateTime TimeStamp{get;set;}
+
+        public Guid Id {get;set;}
     }
 }
