615b19324c7168b424809d97e6a6e5e8f359f866
Sina Setayesh <s.setayesh@gmail.com>
2020-08-21 21:58:38 +1000

    add person model

---
+++ Models/PersonModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace repo.Models
+{
+    public class Person{
+        public string Name{get;set;}
+
+        public string Family{get;set;}
+
+        public string PhoneNumber {get;set;}
+    }
+}
